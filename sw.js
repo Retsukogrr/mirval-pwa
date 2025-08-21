3c2d7f039592b2771333253dced1e64c71b4a9bd
Retsukogrr <ilyaneyney@gmail.com>
2025-08-21 15:28:05 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+const CACHE_VERSION='mirval-v3';
+const STATIC_ASSETS=['./','./aventure.html','./manifest.webmanifest','./sw.js','./icons/icon-192.png','./icons/icon-512.png','./offline.html'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_VERSION).then(c=>c.addAll(STATIC_ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_VERSION?caches.delete(k):Promise.resolve()))));self.clients.claim();});
+self.addEventListener('fetch',e=>{const req=e.request;if(req.mode==='navigate'){e.respondWith(caches.match('./aventure.html').then(cached=>cached||fetch(req).catch(()=>caches.match('./offline.html'))));return;}
+e.respondWith(caches.match(req).then(cached=>cached||fetch(req).then(res=>{const copy=res.clone();caches.open(CACHE_VERSION).then(c=>c.put(req,copy));return res;}).catch(()=>cached)));});
